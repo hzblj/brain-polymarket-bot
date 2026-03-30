0e89493c119a6eab3cf10c73c447fdc5d7a3ed41
Jan Blazej <jan.blazej@footshop.cz>
2026-03-30 22:24:35 +0200

    fix: API gateway

--- apps/agent-gateway-service/src/agent-gateway.module.ts
+++ apps/agent-gateway-service/src/agent-gateway.module.ts
@@ -1,8 +1,10 @@
+import { EventBusModule } from '@brain/events';
 import { Module } from '@nestjs/common';
 import { AgentGatewayController } from './agent-gateway.controller';
 import { AgentGatewayService } from './agent-gateway.service';
 
 @Module({
+  imports: [EventBusModule],
   controllers: [AgentGatewayController],
   providers: [AgentGatewayService],
   exports: [AgentGatewayService],
--- apps/feature-engine-service/src/feature-engine.module.ts
+++ apps/feature-engine-service/src/feature-engine.module.ts
@@ -1,8 +1,10 @@
+import { EventBusModule } from '@brain/events';
 import { Module } from '@nestjs/common';
 import { FeatureEngineController } from './feature-engine.controller';
 import { FeatureEngineService } from './feature-engine.service';
 
 @Module({
+  imports: [EventBusModule],
   controllers: [FeatureEngineController],
   providers: [FeatureEngineService],
   exports: [FeatureEngineService],
